2d25225156cef5367eb99b6f7100812232b3a1c6
nabeelkausari <nabeelkausari@gmail.com>
2018-03-03 12:44:56 +0530

    make set of 100

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import Img from 'react-image';
 import './App.css';
-import { Card, CardText, CardBody, Button, Container, Row, Col, ListGroup, ListGroupItem } from 'reactstrap';
+import { Card, CardText, CardBody, Container, Row, Col, ListGroup, ListGroupItem, Label, Input, FormGroup } from 'reactstrap';
 
 import fetch from 'node-fetch';
+
+const apiUrl = "/";
 
 class App extends Component {
   state = {
     term: '',
     images: [],
-    list: []
+    list: [],
+    listSet: {}
   }
   makeSearch(term) {
-    fetch('/api?term='+ term || this.state.term)
+    fetch(apiUrl + 'api?term='+ term || this.state.term)
       .then(res => res.json())
       .then(images => this.setState({images}))
   }
@@ -22,9 +25,29 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('/file')
+    fetch(apiUrl + 'file')
       .then(res => res.json())
-      .then(list => this.setState({ list }))
+      .then(list => {
+        this.setState({listSet: this.makeListSet(list)});
+        this.setList(1)
+      });
+  }
+
+  makeListSet = (list) => {
+    let ret = {};
+    let chunk = [];
+    for (let item of list) {
+      chunk.push(item);
+      if (chunk.length === 100) {
+        ret[chunk[0][0]] = chunk;
+        chunk = [];
+      }
+    }
+    return ret;
+  }
+
+  setList = (key) => {
+    this.setState({ list: this.state.listSet[key]})
   }
 
   saveImage (img) {
@@ -32,29 +55,31 @@ class App extends Component {
     let fileName = file[file.length - 1];
     let playerName = this.state.term;
 
-    fetch(`/saveImage?fileName=${fileName}&playerName=${playerName}`)
+    fetch(`${apiUrl}saveImage?fileName=${fileName}&playerName=${playerName}`)
       .then(res => res.json());
   }
 
   render() {
-    let {term, images, list} = this.state;
+    let {images, list, listSet} = this.state;
     return (
       <Container className="App" style={{ marginTop: 70 }}>
         <Row>
           <Col xs={3}>
-            <ListGroup style={{ height: 638, overflowY: "scroll" }}>
+            <SelectRows set={listSet} setList={this.setList.bind(this)} />
+            <ListGroup style={{ height: 700, overflowY: "scroll" }}>
               {list.map(item => (
                 <ListGroupItem
+                  key={item[0]}
                   active={item[1] === this.state.term}
                   onClick={() => this.startSearch(item[1])}
-                >{item[1]}</ListGroupItem>)
+                >{item[0]} - {item[1]}</ListGroupItem>)
               )}
             </ListGroup>
           </Col>
           <Col xs={9}>
             <div>
               <ul>
-                {images.map(img => <Card style={{ display: "inline-block", marginBottom: 10 }}>
+                {images.map((img, i) => <Card key={i} style={{ display: "inline-block", marginBottom: 10 }}>
                   <Img src={img.contentUrl} style={{ height: 150 }}/>
                   <CardBody>
                     <CardText>{img.height} x {img.width}</CardText>
@@ -73,5 +98,13 @@ class App extends Component {
     );
   }
 }
+
+const SelectRows = ({ set, setList }) => (
+  <FormGroup>
+    <Input type="select" name="select" id="exampleSelect" onChange={i => setList(i.target.value)}>
+      {Object.keys(set).map(item => <option key={item}>{set[item][0][0]}</option>)}
+    </Input>
+  </FormGroup>
+)
 
 export default App;
